f3e3160256dc2ae28df0ad6361a728a75b0b1c43
unknown <colost72@gmail.com>
2022-09-17 20:42:50 -0600

    Added Post Button to AddPost component

--- src/Components/AddPost/AddPostForm.jsx
+++ src/Components/AddPost/AddPostForm.jsx
@@ -1,12 +1,18 @@
+import React, { useState } from 'react';
 
 
 const AddPostForm = (props) => {
+
+    const [name, setName] = useState(0);
+    const [post, setPost] = useState('');
+
     return (
         <form>
             <label>Name</label>
-            <input type='string' />
+            <input type='text' value={name} onChange={(event) => setName(event.target.value)} />
             <label>Post</label>
-            <input type='string' />
+            <input type='text' value={post} onChange={(event) => setPost(event.target.value)} />
+            <button type='submit'>Post</button>
         </form>
       );
 }
