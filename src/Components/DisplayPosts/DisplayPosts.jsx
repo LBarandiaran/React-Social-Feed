ce2b09b006e752c4e23153b99502ed6ee4963ee3
unknown <colost72@gmail.com>
2022-09-17 19:34:02 -0600

    Created Components Folder and Changed Background color in Public Index file

---
+++ src/Components/DisplayPosts/DisplayPosts.jsx
@@ -0,0 +1,23 @@
+
+const DisplayPosts  = (props) => {
+    return (
+    <table>
+        <thead>
+          <tr>
+            <th>Entry Number</th>
+            <th>Weight</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>175</td>
+            <td>11-23-2021</td>
+          </tr>
+        </tbody>
+    </table>
+      );
+}
+ 
+export default DisplayPosts;
