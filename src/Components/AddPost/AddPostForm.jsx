bc7b3224b1f7a168d08fa0e835f6dd4c31d1b422
unknown <colost72@gmail.com>
2022-09-17 20:01:44 -0600

    Created AddPost Component

---
+++ src/Components/AddPost/AddPostForm.jsx
@@ -0,0 +1,14 @@
+
+
+const AddPostForm = (props) => {
+    return (
+        <form>
+            <label>Name</label>
+            <input type='string' />
+            <label>Post</label>
+            <input type='string' />
+        </form>
+      );
+}
+ 
+export default AddPostForm;
